1489bede89e930e1fea33b035425cd9902e0f741
mateoalzarigirardi <alzarimateo@gmail.com>
2025-06-10 23:14:23 -0300

    hola

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args){
         Funciones Funciones = new Funciones();
         Funciones.mostrarSaludo();
-        Funciones.saludar();
-        Funciones.areaRectangulo();
-        Funciones.convertirMayusculas();
+
+        Funciones.saludar("Mateo");
+
+        Funciones.areaRectangulo(5, 10);
+
+        Funciones.convertirMayusculas("hola mundo");
+
+        Funciones.mostrarMenu();
 
     }
 }
