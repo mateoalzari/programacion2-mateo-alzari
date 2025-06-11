1ae8f26137ff6adaa941110a0236ac44d740efb6
mateoalzarigirardi <alzarimateo@gmail.com>
2025-06-10 23:09:12 -0300

    hola

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args){
         Funciones Funciones = new Funciones();
         Funciones.mostrarSaludo();
+        Funciones.saludar();
+
     }
 }
