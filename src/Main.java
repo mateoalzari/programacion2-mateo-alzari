5d972d8c5f76de9ddcbe2cc7f29b6574fe738fe8
mateoalzarigirardi <alzarimateo@gmail.com>
2025-06-10 21:49:26 -0300

    hola

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void mostrarSaludo() {
-        System.out.println("Hola, mundo");
+        System.out.println("Hola,mundo");
     }
+    public static void saludar (String nombre) {
+        System.out.println("Hola, "+ nombre);
+    }
+
 
 }
